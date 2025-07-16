c3e6881001491eed82635cdd7c8928d972ac2a5c
BagitDogo <idogofer0@gmail.com>
2025-07-16 23:42:16 +0300

    Update GOG.py

--- GOG.py
+++ GOG.py
@@ -59,7 +59,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # === Serpent’s Hand Logo + Title ===
-st.image("https://static.wikia.nocookie.net/villains/images/a/a5/TheSerpentsHand.png/revision/latest?cb=20171104212729", width=120)
+st.image("https://scpstore.org/cdn/shop/products/kiss-cut-stickers-5.5x5.5-default-61d4bba5170d8.png?v=1649862918", width=120)
 st.title("Serpent's Hand Command Generator")
 st.markdown("---")
 
